7b78d79392ae2a84593c3582dce14112743e1e9e
Usmanov Mansurjon <usmanovmansur@gmail.com>
2023-04-06 10:58:04 +0500

    html

---
+++ web/main.dart
@@ -0,0 +1,6 @@
+import 'dart:html';
+
+void main() {
+  final a = querySelector('#qwert') as AnchorElement;
+  a.href = "https://yandex.uz/search/?text=https%3A%2F%2F%2Fambercheydesigns%2Fpen%2FVwEZpVW&clid=2331541&banerid=6302000000%3A641e6c5d57b99769c3f8f54e&win=586&lr=194600";
+}
